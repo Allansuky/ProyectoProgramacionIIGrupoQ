b7f9779bb1b9e6f7d268130fe8185cd456aec610
quesi <quesi@192.168.1.22>
2025-08-11 00:22:06 -0600

    cambio y correccion de jMICiudad

--- src/main/java/capapresentacion/JFraCiudad.java
+++ src/main/java/capapresentacion/JFraCiudad.java
@@ -187,35 +187,6 @@ public class JFraCiudad extends javax.swing.JInternalFrame {
         }
     }
     
-    public static void obtenerBD() throws InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-        try{
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JFraCiudad.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);  
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JFraCiudad.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JFraCiudad.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JFraCiudad.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    new JFraCiudad().setVisible(true);
-                } catch (SQLException ex) {
-                    System.getLogger(JFraCiudad.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
-                }
-            }
-        });
-    }
-    
     
     
     /**
